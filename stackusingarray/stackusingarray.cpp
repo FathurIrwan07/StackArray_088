e7292134d6bd4e95cdb178889a5b5fd2f9fbe889
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-06-16 17:27:56 +0700

    membuat objek dan memanggil program utama

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -60,3 +60,38 @@ public:
 };
 
 
+int main() {
+	StackArray obj;
+	char ch;
+	while (true) {
+		cout << "\nMenu" << endl;
+		cout << "1. push a element " << endl;
+		cout << "2. pop element" << endl;
+		cout << "3. display " << endl;
+		cout << "4. Exit" << endl;
+		cout << "\nEnter your choice (1-4): ";
+		cin >> ch;
+
+		switch (ch) {
+		case '1': {
+			obj.push();
+			break;
+		}
+		case '2': {
+			obj.pop();
+			break;
+		}
+		case '3': {
+			obj.display();
+			break;
+		}
+		case '4': {
+			return 0;
+		}
+		default: {
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
+}; 
